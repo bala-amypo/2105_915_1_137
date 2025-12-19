ae43c3b5c4cea7a1ed0106aac98fb2696f6faf09
bala-amypo <bala@amypo.in>
2025-12-19 06:28:26 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/impl/AuthServiceImpl.java
@@ -0,0 +1,66 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.dto.AuthRequestDto;
+import com.example.demo.dto.AuthResponseDto;
+import com.example.demo.dto.RegisterRequestDto;
+import com.example.demo.entity.UserAccount;
+import com.example.demo.exception.BadRequestException;
+import com.example.demo.repository.UserAccountRepository;
+import com.example.demo.security.JwtUtil;
+import com.example.demo.service.AuthService;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthServiceImpl implements AuthService {
+
+    private final UserAccountRepository userRepo;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
+    private final AuthenticationManager authManager;
+
+    public AuthServiceImpl(
+            UserAccountRepository userRepo,
+            PasswordEncoder passwordEncoder,
+            JwtUtil jwtUtil,
+            AuthenticationManager authManager) {
+        this.userRepo = userRepo;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtUtil = jwtUtil;
+        this.authManager = authManager;
+    }
+
+    @Override
+    public AuthResponseDto login(AuthRequestDto request) {
+
+        authManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+
+        String token = jwtUtil.generateToken(request.getEmail());
+        return new AuthResponseDto(token);
+    }
+
+    @Override
+    public void register(RegisterRequestDto request) {
+
+        if (userRepo.existsByEmail(request.getEmail())) {
+            throw new BadRequestException("Email already exists");
+        }
+
+        UserAccount user = new UserAccount();
+        user.setEmail(request.getEmail());
+        user.setFullName(request.getFullName());
+        user.setPassword(
+                passwordEncoder.encode(request.getPassword()));
+        user.setActive(true);
+
+        userRepo.save(user);
+    }
+}
