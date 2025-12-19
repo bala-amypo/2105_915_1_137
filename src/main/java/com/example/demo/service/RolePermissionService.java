63b9d4b2796208d2c55610cf5589a482c3abc23f
bala-amypo <bala@amypo.in>
2025-12-19 09:02:06 +0000

    User commit

--- src/main/java/com/example/demo/service/RolePermissionService.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.service;
-
-import com.example.demo.entity.RolePermission;
-
-public interface RolePermissionService {
-
-    RolePermission assignPermissionToRole(Long roleId, Long permissionId);
-}
--- src/main/java/com/example/demo/service/UserRoleService.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.service;
-
-import com.example.demo.entity.UserRole;
-
-public interface UserRoleService {
-
-    UserRole assignRoleToUser(Long userId, Long roleId);
-}
