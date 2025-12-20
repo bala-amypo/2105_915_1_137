8bce34998b18c65cea6206b16f7611c8284f3689
bala-amypo <bala@amypo.in>
2025-12-20 15:23:20 +0000

    User commit

--- src/main/java/com/example/demo/entity/Role.java
+++ src/main/java/com/example/demo/entity/Role.java
@@ -1,24 +1,40 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+
 @Entity
+@Table(name = "roles")
 public class Role {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String roleName;
-    private String description;
+    private String name;
+
     private boolean active = true;
 
+    // getters & setters
     public Long getId() {
         return id;
     }
+
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public boolean isActive() {
         return active;
     }
+
     public void setActive(boolean active) {
         this.active = active;
     }
