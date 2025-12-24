3db4f9670eaf491904253880d4e91ec7290fd2df
bala-amypo <bala@amypo.in>
2025-12-24 06:06:15 +0000

    User commit

---
+++ src/main/java/com/example/demo/dto/AuthResponseDto.java
@@ -0,0 +1,9 @@
+package com.example.demo.dto;
+
+public class AuthResponseDto {
+    private String token;
+
+    public AuthResponseDto(String token) { this.token = token; }
+    public String getToken() { return token; }
+    public void setToken(String token) { this.token = token; }
+}
