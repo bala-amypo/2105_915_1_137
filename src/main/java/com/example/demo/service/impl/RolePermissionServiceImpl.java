570e4150d87615d602dba9e69e6700641820fa16
bala-amypo <bala@amypo.in>
2025-12-24 12:54:49 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/RolePermissionServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/RolePermissionServiceImpl.java
@@ -1,12 +1,10 @@
 package com.example.demo.service.impl;
 
-import com.example.demo.entity.RolePermission;
-import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.repository.RolePermissionRepository;
+import com.example.demo.repository.RoleRepository;
+import com.example.demo.repository.PermissionRepository;
 import com.example.demo.service.RolePermissionService;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 public class RolePermissionServiceImpl implements RolePermissionService {
--- src/main/java/com/example/demo/service/impl/UserRoleServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserRoleServiceImpl.java
@@ -1,13 +1,10 @@
 package com.example.demo.service.impl;
 
-import com.example.demo.entity.UserRole;
-import com.example.demo.exception.BadRequestException;
-import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.repository.UserRoleRepository;
+import com.example.demo.repository.UserAccountRepository;
+import com.example.demo.repository.RoleRepository;
 import com.example.demo.service.UserRoleService;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 public class UserRoleServiceImpl implements UserRoleService {
