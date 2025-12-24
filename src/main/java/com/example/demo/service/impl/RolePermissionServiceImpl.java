cb6ec139064ec08734598277f7a2bf6db80355e1
bala-amypo <bala@amypo.in>
2025-12-24 06:14:37 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/impl/RolePermissionServiceImpl.java
@@ -0,0 +1,37 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.entity.RolePermission;
+import com.example.demo.exception.ResourceNotFoundException;
+import com.example.demo.repository.PermissionRepository;
+import com.example.demo.repository.RolePermissionRepository;
+import com.example.demo.repository.RoleRepository;
+import com.example.demo.service.RolePermissionService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class RolePermissionServiceImpl implements RolePermissionService {
+    private final RolePermissionRepository rolePermissionRepository;
+    private final RoleRepository roleRepository;
+    private final PermissionRepository permissionRepository;
+
+    public RolePermissionServiceImpl(RolePermissionRepository rolePermissionRepository,
+                                   RoleRepository roleRepository,
+                                   PermissionRepository permissionRepository) {
+        this.rolePermissionRepository = rolePermissionRepository;
+        this.roleRepository = roleRepository;
+        this.permissionRepository = permissionRepository;
+    }
+
+    @Override
+    public List<RolePermission> getPermissionsForRole(Long roleId) {
+        return rolePermissionRepository.findByRole_Id(roleId);
+    }
+
+    @Override
+    public RolePermission getMappingById(Long id) {
+        return rolePermissionRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Role permission mapping not found"));
+    }
+}
