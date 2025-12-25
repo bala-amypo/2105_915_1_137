cb056e1b37642320417074fb1c020389622c8bf2
bala-amypo <bala@amypo.in>
2025-12-25 17:20:26 +0000

    User commit

--- src/main/java/com/example/demo/servlet/SimpleStatusServlet.java
+++ src/main/java/com/example/demo/servlet/SimpleStatusServlet.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 public class SimpleStatusServlet extends HttpServlet {
 
     @Override
-    protected void doGet(HttpServletRequest req,
+    public void doGet(HttpServletRequest req,
                          HttpServletResponse resp)
             throws IOException {
 
