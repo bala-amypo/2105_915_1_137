c2882834d9232934061648f597a690e8dd01a456
bala-amypo <bala@amypo.in>
2025-12-30 04:08:52 +0000

    User commit

--- src/main/java/com/example/demo/entity/UserAccount.java
+++ src/main/java/com/example/demo/entity/UserAccount.java
@@ -36,7 +36,7 @@ public class UserAccount {
         this.updatedAt = Instant.now();
     }
     
-    // Getters and setters
+    
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
     
